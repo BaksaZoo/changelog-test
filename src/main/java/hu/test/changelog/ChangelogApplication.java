af5a5f8d33a590d40e6702f76f254d3ffeb867b2
BaksaZoo <baksazoo@hotmail.com>
2024-04-25 19:25:55 +0200

    OL-1000: init project

---
+++ src/main/java/hu/test/changelog/ChangelogApplication.java
@@ -0,0 +1,13 @@
+package hu.test.changelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangelogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/test/changelog/ChangelogApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.test.changelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangelogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
