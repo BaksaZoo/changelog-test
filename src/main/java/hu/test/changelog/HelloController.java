b55262562aea5fdc27bdbc946bba02aaf6b9e90e
BaksaZoo <baksazoo@hotmail.com>
2024-04-25 19:53:43 +0200

    OL-1001: say hello by name

--- src/main/java/hu/test/changelog/HelloController.java
+++ src/main/java/hu/test/changelog/HelloController.java
@@ -1,13 +1,16 @@
 package hu.test.changelog;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping("/")
-    public ResponseEntity<String> hello() {
-        return ResponseEntity.ok("Hello World");
+    @GetMapping("/{name}")
+    public ResponseEntity<String> hello(@PathVariable String name) {
+        name = StringUtils.hasText(name) ? "World" : name;
+        return ResponseEntity.ok("Hello " + name);
     }
 }
